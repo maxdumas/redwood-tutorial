ce109877e0f08ecb2a1661cea47e552d9df4c805
Max Dumas <maltor124@gmail.com>
2021-06-19 06:58:50 -0400

    Display blog posts on Home Page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,9 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <>
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <BlogPostsCell />
     </>
   )
 }
